be7e4e14582ab3bd484921e2f7ca1204ba499d38
Prabhat <krsinghp8@gmail.com>
2022-10-12 11:47:14 +0530

    updates

--- 02 Bit Magic/02_Count_set_bits.java
+++ 02 Bit Magic/02_Count_set_bits.java
@@ -1,7 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 class Count_set_bits {
     static int count_set_bits_naive(int n){
-        int count =0;int x;
+        int count =0;
 
+        //int x;
         while(n>0)
         {
             // if(n%2!=0)
@@ -20,8 +24,44 @@ class Count_set_bits {
         return count;
     }
 
+    static int count_set_bits_brian_kernigam_method(int n)
+    {
+        int res=0;int x;
+        while(n>0)
+        {
+            n=(n&(n-1));
+            res++;
+        }
+        return res;
+    }
+
+    static int count_set_bits_lookup_table_method(int n)
+    {
+        int[] table= new int[256];
+        Arrays.fill(table, 0);
+
+        for(int i=0; i<256;i++)
+        {
+            table[i] = (i&1) + table[i/2];
+        }
+
+        int res = table[n&0xff];
+        n=n>>8;
+        res = res+ table[n&0xff];
+        n=n>>8;
+        res = res+ table[n&0xff];
+        n=n>>8;
+        res = res+ table[n&0xff];
+
+        return res;
+
+    }
+
     public static void main(String[] args) {
-        
+        int x=10;
+        System.out.println(count_set_bits_naive(x));
+        System.out.println(count_set_bits_brian_kernigam_method(x));
+        System.out.println(count_set_bits_lookup_table_method(x));
     }
     
 }
