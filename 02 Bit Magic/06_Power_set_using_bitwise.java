f4f889b711b86553916013da4a1c92dd7ca489e7
Prabhat <krsinghp8@gmail.com>
2022-10-17 11:22:34 +0530

    power set

--- 02 Bit Magic/06_Power_set_using_bitwise.java
+++ 02 Bit Magic/06_Power_set_using_bitwise.java
@@ -1,6 +1,21 @@
 class power_set_using_bitwise{
 
+    static void Power_set_using_bitwise(String str){
+        int n= str.length(); int x;
+        int nPow = (int)Math.pow(2, n);
+
+        for(int i=0; i<nPow; i++){
+
+            for(int j=0; j<n;j++){
+                x=i&(1<<j);
+                if(x!=0)
+                System.out.print(str.charAt(j));
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
-        
+        Power_set_using_bitwise("abc");
     }
 }
